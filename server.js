713206387d0ef5f3672df7b98d9ff708d3fc602a
gabrielmosca2002-star <gabrielmosca2002@gmail.com>
2026-07-17 07:28:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ const pool = new Pool({
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const hdrs = () => ({ 'authorization': `Token ${SHOPPUB_TOKEN}`, 'accept': 'application/json' });
 
